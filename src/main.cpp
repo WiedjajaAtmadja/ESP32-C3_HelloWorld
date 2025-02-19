461c4d0e00050d9f9ff4544acdf98e5520888f0c
Wiedjaja Atmadja <swiedjaja@binus.ac.id>
2025-02-19 14:36:13 +0700

    add wokwi

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,34 @@
 // ESP32-C3 CoreBoard: https://wiki.luatos.org/chips/esp32c3/board.html
 #include <Arduino.h>
-#include <HWCDC.h>
-#define LED_D4 12
-#define LED_D5 13
+// #include <HWCDC.h>
+#define LED_D4 0
+// #define LED_D5 13
 
-HWCDC cdcSerial;
+// HWCDC cdcSerial;
 void setup() {
   pinMode(LED_D4, OUTPUT);
-  pinMode(LED_D5, OUTPUT);
-  cdcSerial.begin(9600);
+  // pinMode(LED_D5, OUTPUT);
+  digitalWrite(LED_D4, LOW);
+  // digitalWrite(LED_D5, LOW);
+  Serial.begin(9600);
 }
 
 void loop() {
-  cdcSerial.println("Hello World");
-  digitalWrite(LED_D4, HIGH);
-  digitalWrite(LED_D5, LOW);
-  delay(100);
-  digitalWrite(LED_D4, LOW);
-  digitalWrite(LED_D5, HIGH);
+  Serial.println("Hello World");
+  for (int i=0; i<1024; i++){
+    Serial.print(".");
+    analogWrite(LED_D4, i);
+    delay(50);
+  }
   delay(1000);
+  // for (int i=1023; i>=0; i--){
+  //   analogWrite(LED_D4, i);
+  //   delay(50);
+  // }
+  // digitalWrite(LED_D4, HIGH);
+  // digitalWrite(LED_D5, LOW);
+  // delay(100);
+  // digitalWrite(LED_D4, LOW);
+  // digitalWrite(LED_D5, HIGH);
+  // delay(1000);
 }
